7208feafdc3e23ee913d589dd2fb85be4806e690
mw18 <67554339+mw18@users.noreply.github.com>
2021-06-05 15:09:51 -0500

    clear the input field

--- index.js
+++ index.js
@@ -3,15 +3,23 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-inputBtn.addEventListener("click", function() {
-    
+inputBtn.addEventListener("click", function() {  
     myLeads.push(inputEl.value)
-    console.log(myLeads)
-
-    for (let i = 0; i < myLeads.length; i++){
-        ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-    }
-
+    inputBtn.value = ""
+    renderLeads()
 })
 
 
+function renderLeads(){
+
+    let listItems = ""
+    for ( let i = 0; i <MediaElementAudioSourceNode.length; i++){
+        listItems += "<li>" + myLeads[i] + "</li>"
+    }
+    ulEl.innerHTML = listItems
+}
+
+//Use createElement() and append() instead of innerHTML
+// const li = document.createElement("li")
+// li.textContent = myLeads[i]
+// ulEl.append(li)
